4847e96608deaad115484fe07eeef36dfc05c095
Garth Hill <thaabit@gmail.com>
2016-02-04 14:11:34 -0700

    add fixsshauth

--- symlink.sh
+++ symlink.sh
@@ -6,30 +6,36 @@
 
 ########## Variables
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim screenrc"    # list of files/folders to symlink in homedir
+
+dotdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+if [ ! -e $olddir ]; then
+    echo "creating $olddir"
+    error=`mkdir -p $olddir 2>&1`
+    if [ "$error" ]; then echo $error; fi
+fi
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     if [ ! -h ~/.$file ]; then
         if [ -e ~/.$file ]; then
             echo "moving existing .$file to ~/$olddir"
-            mv ~/.$file ~/dotfiles_old/
+            error=`mv ~/.$file ~/dotfiles_old/ 2>&1`
+            if [ "$error" ]; then echo $error; fi
         fi
         echo "creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.$file
+        error=`ln -s $dotdir/$file ~/.$file 2>&1`
+        if [ "$error" ]; then echo $error; fi
     fi
 done
+
+if [ ! -e /usr/bin/fixsshauth ]; then
+    echo "creating symlink to fixsshauth in /usr/bin."
+    error=`sudo ln -s $dotdir/fixsshauth /usr/bin/fixsshauth 2>&1`
+    if [ "$error" ]; then echo $error; fi
+fi
